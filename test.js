6c295fa51b5c9ebe6bdd18e400369b139798562f
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-04-10 12:05:59 +0530

    Simple SASL test support

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-var tcp = require("tcp");
+var net = require("net");
 var sys = require("sys");
 var xml = require("libxmljs");
+var sasl = require("sasl");
 
 var hashAttribute = function( attr ) {
     return { name : attr[0]
@@ -18,10 +19,6 @@ var Session = function() {
     var self = this;
     this.parser = new xml.SaxPushParser(function(cb) {
         cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
-            sys.debug("Namespaces" );
-            sys.debug(sys.inspect(namespaces));
-            sys.debug("Attributes");
-            sys.debug(sys.inspect(hashAttributes(attrs)));
         });
         cb.onEndElementNS(function() {
         });
@@ -36,22 +33,74 @@ Session.prototype = {
     }
 }
 
-var server = tcp.createServer(function (socket) {
+/*
+ * If callback returns null
+ * assumed that callback doesn't want to return anything
+ * otherwise whatever is returned is passed on
+ */
+var cb = function(  property, sess ) {
+    sys.debug("Session obj is " + sys.inspect(sess));
+    if( property == sasl.GSASL_PASSWORD
+      && sess.property("authid") == "nikhil" ) {
+          sys.debug("OK!!");
+          sess.setProperty( "password", "1234" );
+          return sasl.GSASL_OK;
+    }
+    if( property == sasl.GSASL_REALM )
+        return "localhost";
+
+}
+
+var server = net.createServer(function (socket) {
   socket.setEncoding("utf8");
 
   var ses;
+  var sc;
+  var cr = false; // challenge written
   socket.addListener("connect", function () {
       ses = new Session();
+      sc = sasl.createServerSession( "localhost", cb );
+      socket.write( "<stream:stream xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' id='1234' from='localhost' version='1.0' xml:lang='en'>" );
+      socket.write( "<stream:features><mechanisms xmlns='urn:ietf:params:xml:ns:xmpp-sasl'>" );
+      sc.mechanisms.forEach(function(i) {
+          socket.write( "<mechanism>" + i + "</mechanism>" );
+      });
+      socket.write( "</mechanisms></stream:features>" );
   });
   socket.addListener("data", function (data) {
-      sys.debug(data);
       ses.handle(data);
  
-      socket.write( "<stream:stream xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' id='1234' from='localhost' version='1.0' xml:lang='en'>" );
-      socket.write( "<stream:features> <bind xmlns='urn:ietf:params:xml:ns:xmpp-bind'/> <session xmlns='urn:ietf:params:xml:ns:xmpp-session'/> </stream:features>");
+      if( !cr ) {
+          sc.start("DIGEST-MD5");
+          cr = true;
+          return;
+      }
+      var res;
+      sys.debug("Data from client " + data);
+      var s = /<response xmlns="urn:ietf:params:xml:ns:xmpp-sasl">(.*)<\/response>/.exec(data);
+      if ( s == null ) {
+          if( /<response .*\/>/.exec(data) ) {
+                socket.write( "<success xmlns='urn:ietf:params:xml:ns:xmpp-sasl'/>" );
+          }
+          else {
+              res = sc.step( "" );
+          }
+      }
+      else {
+          res = sc.step( s[1] );
+      }
+      sys.debug(sys.inspect(res));
+      if( res.data == undefined )
+          process.exit(1);
+      if( res.status != sasl.GSASL_OK && res.status != sasl.GSASL_NEEDS_MORE ) {
+        process.exit(1);
+      }
+      socket.write( "<challenge xmlns='urn:ietf:params:xml:ns:xmpp-sasl'>" + res.data + "</challenge>");
+      //socket.write( "<stream:features> <bind xmlns='urn:ietf:params:xml:ns:xmpp-bind'/> <session xmlns='urn:ietf:params:xml:ns:xmpp-session'/> </stream:features>");
   });
   socket.addListener("end", function () {
-    socket.close();
+    socket.end();
   });
 });
+sys.debug(sys.inspect(server));
 server.listen(5222, "localhost");
