5fb757e235edcb3b57537a13c12d4a30f22f5774
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-04-13 22:47:29 +0530

    Added basic stanza parser.
    Uses a push parser and generates a object heirarchy like Python's ElementTree.
    Emits on receiving a stanza. Not meant for use with beginning a stream.
    
    It also doesn't handle multiple children with the same node name as required
    for <presence/> for example ( http://xmpp.org/rfcs/rfc3921.html#rfc.section.2.2.2.2 )

---
+++ core/parser.js
@@ -0,0 +1,118 @@
+var net = require("net");
+var sys = require("sys");
+var xml = require("libxmljs");
+var sasl = require("sasl");
+var events = require('events');
+
+Function.prototype.bind = (function() {
+  var _slice = Array.prototype.slice;
+  return function(context) {
+    var fn = this,
+        args = _slice.call(arguments, 1);
+    
+    if (args.length) { 
+      return function() {
+        return arguments.length
+          ? fn.apply(context, args.concat(_slice.call(arguments)))
+          : fn.apply(context, args);
+      }
+    } 
+    return function() {
+      return arguments.length
+        ? fn.apply(context, arguments)
+        : fn.call(context);
+    }; 
+  }
+})();
+
+var buildAttr = function( attr ) {
+    return { name : attr[0]
+           , prefix : attr[1]
+           , uri : attr[2]
+           , value : attr[3]
+           };
+}
+
+var buildAttrs = function( attrs ) {
+    var a = {};
+    attrs.forEach( function(i) {
+        var attr = buildAttr( i );
+        a[attr.name] = attr;
+    } );
+    return a;
+}
+
+/**
+ * A XMPP parser
+ * Assumes SASL handling is done
+ *
+ * Create an instance and add listeners
+ * var p = new parser.Parser();
+ * p.addListener( "streamOpen", function(attrs) { ... } )
+ * p.addListener( "streamClose", function() { ... } )
+ * p.addListener( "stanza", function( stanza ) { ... } );
+ * p.addListener( "error", function( errmsg ) { ... } );
+ *
+ * // use
+ * p.parse( data );
+ */
+
+exports.Parser = function() {
+    events.EventEmitter.call(this);
+    this._tagStack = [];
+    var self = this;
+    this.parser = new xml.SaxPushParser( function(cb) {
+        cb.onStartElementNS( self.startElement.bind(self) );
+        cb.onEndElementNS( self.endElement.bind(self) );
+        cb.onCharacters( self.onCharacters.bind(self) );
+        cb.onError( self._errorHandler.bind(self) );
+    } );
+}
+
+sys.inherits( exports.Parser, events.EventEmitter );
+
+exports.Parser.prototype._errorHandler = function( err ) {
+    this.emit( 'error', err );
+},
+
+exports.Parser.prototype.startElement = function( elem, attrs, prefix, uri, namespaces ) {
+    tag = {};
+    tag.name = elem;
+    tag.attrs = buildAttrs( attrs );
+    tag.prefix = prefix;
+    tag.uri = uri;
+    tag.namespaces = namespaces;
+
+    this._tagStack.push( tag );
+},
+
+exports.Parser.prototype.endElement = function( elem, prefix, uri ) {
+    var tag = this._tagStack.pop();
+    if( this._tagStack.length == 0 ) {
+        // time to emit a stanza
+        this.emit( 'stanza', tag );
+    }
+    else {
+        // put this as the child of parent
+        var last = this._tagStack[this._tagStack.length-1];
+        if( !last.children )
+            last.children = {};
+        last.children[tag.name] = tag;
+    }
+},
+
+exports.Parser.prototype.onCharacters = function(chars) {
+    sys.log(chars);
+    if( this._tagStack.length == 0 ) {
+        return;
+    }
+
+    var top = this._tagStack[this._tagStack.length-1];
+    if( !top.text )
+        top.text = "";
+    top.text += chars;
+},
+
+exports.Parser.prototype.parse = function( data ) {
+    this.parser.push( data );
+}
---
+++ tests/parser.js
@@ -0,0 +1,29 @@
+var sys = require('sys');
+var assert = require('assert');
+var parser = require('../core/parser');
+
+var p = new parser.Parser();
+
+p.addListener( "error", function(msg) {
+    sys.log( "TEST ERROR "+ msg );
+} );
+
+p.addListener( "stanza", function(stanza) {
+    assert.equal( stanza.name, 'message', "Wrong stanza name, expected message" );
+    assert.equal( Object.keys(stanza.attrs).length, 4, "wrong no. of attributes" );
+    assert.ok( stanza.children['body'] );
+    assert.equal( stanza.attrs['from'].value, 'juliet@example.com/balcony' );
+});
+
+p.parse( "<message to='romeo@example.net' from='juliet@example.com/balcony' type='chat' xml:lang='en'> <body>Wherefore art thou, Romeo?</body> </message>");
+
+// --------------
+
+var p2 = new parser.Parser();
+p2.addListener( "stanza", function(stanza) {
+    assert.equal( stanza.name, 'presence', "Wrong stanza name, expected presence" );
+    assert.equal( Object.keys(stanza.attrs).length, 1, "wrong no. of attributes" );
+    sys.debug(sys.inspect(stanza));
+});
+
+p2.parse( "<presence xml:lang='en'><show>dnd</show><status>Wooing Juliet</status><status xml:lang='cz'>Ja dvo&#x0159;&#x00ED;m Juliet</status></presence>" );
