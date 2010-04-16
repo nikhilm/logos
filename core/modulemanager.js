aef271d86d18bf489f3d3a041c2a36eaa8cf9b8b
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-04-16 13:13:12 +0530

    Module manager handles simple stanza events. Requires complete object hashing and not just the name

--- core/modulemanager.js
+++ core/modulemanager.js
@@ -2,9 +2,33 @@ var sys = require('sys');
 
 var eventbus = require('core/eventbus').instance;
 
+stanzaHandlers = {};
+
+var stanzaHandler = function(session, stanza) {
+    if( stanzaHandlers[stanza.name] ) {
+        stanzaHandlers[stanza.name].forEach(function(func) {
+            func(session, stanza);
+        });
+    }
+}
+
+eventbus.addListener("stanza", stanzaHandler);
+
+exports.load = function(name) {
+    require('../plugins/' + name);
+}
+
 // -----------------------
 // API exposed to modules
+// TODO: documentation
 
 exports.hook = function(evt, f) {
     eventbus.addListener(evt, f);
+}
+
+exports.handleStanza = function(stanza_name, func) {
+    if( !stanzaHandlers[stanza_name] )
+        stanzaHandlers[stanza_name] = [];
+
+    stanzaHandlers[stanza_name].push(func);
 }
